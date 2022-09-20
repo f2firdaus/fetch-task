f6ce492482e9839cfe94a922da5b26ae30684ffe
f2firdaus <firdausnaseem@gmail.com>
2022-09-20 17:28:24 +0530

    git-task

--- index.js
+++ index.js
@@ -2,31 +2,6 @@ const btn = document.getElementById('btn');
 
 
 
-// btn.addEventListener('click', () => {
-//     const pr = fetch('https://reqres.in/api/users');
-//     pr.then(response => {
-//         const pr1 = response.json();
-//         return pr1;
-//     }).then(data => {
-//         console.log(data);
-//         const another_pr = fetch('https://reqres.in/api/users?page=2');
-//         another_pr.then(re => {
-//             const another_j = re.json();
-//             return another_j
-//         }).then(data => {
-//             console.log(data);
-//         })
-//     })
-// })
-
-// btn.addEventListener('click', _ => {
-//     fetch('https://reqres.in/api/users').then(r => r.json())
-//         .then(data => {
-//             console.log(data);
-//     return fetch('https://reqres.in/api/users?page=2');
-//         })
-//         .then(re => re.json()).then(dat => console.log(dat));
-// })
 
 const render = data => {
     const list = data;
